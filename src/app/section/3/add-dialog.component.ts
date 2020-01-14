be6e4a5ba50ed89d53657e92d2d81c4eeb0b18e0
Mieky Sofyan Yudinata <miki211287@gmail.com>
2020-01-14 15:05:48 +0700

    .

--- src/app/section/3/add-dialog.component.ts
+++ src/app/section/3/add-dialog.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { OrdersComponent } from './advanced-data';
+import { OrdersComponent, Item } from './advanced-data';
 import { FormArray, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
@@ -10,9 +10,8 @@ import { FormArray, FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class AddDialogComponent implements OnInit {
   form: FormGroup;
-  items: FormArray;
-  itemList: Map<string, any>;
-  itemIndex: number;
+  itemList: Map<string, Item[]>;
+  priceList: Map<string, Map<string, number>>;
 
   constructor(
     private fb: FormBuilder,
@@ -23,8 +22,44 @@ export class AddDialogComponent implements OnInit {
     return this.fb.group({
       category: ['', Validators.required],
       name: ['', Validators.required],
-      price: ['', Validators.required]
+      price: ['']
     })
+  }
+
+  initItemList(): void {
+    this.itemList = new Map();
+    this.itemList.set('CPU', this.orderComponent.cpuList);
+    this.itemList.set('Motherboard', this.orderComponent.motherBoardList);
+    this.itemList.set('Video Card', this.orderComponent.videoCardList);
+    this.itemList.set('Memory', this.orderComponent.memoryList);
+  }
+
+  initPriceList(): void {
+    this.priceList = new Map();
+    let prices: Map<string, number> = new Map();
+    for (let p of this.orderComponent.cpuList) {
+      prices.set(p.name, p.price);
+    }
+    this.priceList.set('CPU', prices);
+
+    prices = new Map();
+    for (let p of this.orderComponent.motherBoardList) {
+      prices.set(p.name, p.price);
+    }
+    this.priceList.set('Motherboard', prices);
+
+    prices = new Map();
+    for (let p of this.orderComponent.videoCardList) {
+      prices.set(p.name, p.price);
+    }
+    this.priceList.set('Video Card', prices);
+
+    prices = new Map();
+    for (let p of this.orderComponent.memoryList) {
+      prices.set(p.name, p.price);
+    }
+    this.priceList.set('Memory', prices);
+
   }
 
   ngOnInit(): void {
@@ -33,24 +68,28 @@ export class AddDialogComponent implements OnInit {
       email: ['', [Validators.required, Validators.email]],
       items: this.fb.array([ this.createItem() ])
     });
-    this.itemList = new Map();
-    this.itemList.set('CPU', this.orderComponent.cpuList);
-    this.itemList.set('Motherboard', this.orderComponent.motherBoardList);
-    this.itemList.set('Video Card', this.orderComponent.videoCardList);
-    this.itemList.set('Memory', this.orderComponent.memoryList);
+
+    this.initItemList();
+    this.initPriceList();
   }
 
-  /* get items(): FormArray {
-    return this.form.get('items') as FormArray;
-  } */
-
   addItem(): void {
-    this.items = this.form.get('items') as FormArray;
-    this.items.push(this.createItem());
+    const items: FormArray = this.form.get('items') as FormArray;
+    items.push(this.createItem());
   }
 
   removeItem(index: number): void {
-    this.items.removeAt(index);
+    const items: FormArray = this.form.get('items') as FormArray;
+    items.removeAt(index);
+  }
+
+  updateName(i: number, item: Item): void {
+    this.form['controls']['items']['controls'][i]['controls'].name.patchValue(item.name);
+    this.form['controls']['items']['controls'][i]['controls'].price.patchValue(item.price);
+  }
+
+  updatePrice(i: number, price: number): void {
+    this.form['controls']['items']['controls'][i]['controls'].price.patchValue(price);
   }
 
   onNoClick(): void {
